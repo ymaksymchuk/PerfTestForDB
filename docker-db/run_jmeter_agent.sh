07f77a18a61b35f9d7794b0a86a1595a253e6186
ymaksymchuk <yehor.maksymchuk@avenga.com>
2020-03-02 14:27:54 +0200

    Finalize created tests

--- docker-db/run_jmeter_agent.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-
-echo "Starting Jmeter Agent"
-echo "jdk8u242jre/bin/java -jar ServerAgent/CMDRunner.jar --tool PerfMonAgent"
-
-./jdk8u242jre/bin/java -jar ServerAgent/CMDRunner.jar --tool PerfMonAgent
--- jmeter_run.sh
+++ jmeter_run.sh
@@ -1,2 +1,25 @@
 #!/usr/bin/env bash
-java -jar ./jmeter/bin/ApacheJMeter.jar -t ./postgresql_project_example.jmx -l ./testresults.jtl
+#java -jar ./jmeter/bin/ApacheJMeter.jar -n -f -t ./jmeter_test_cases/PostgreSQL/TestCase#4_Postgre.jmx -l ./jmeter_test_result/PostgreSQL/TC#1/general_test_result.jtl -e -o ./jmeter_reports/PostgreSQL/TC#1/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -n -f -t ./jmeter_test_cases/PostgreSQL/TestCase#2_Postgre.jmx -l ./jmeter_test_result/PostgreSQL/TC#2/general_test_result.jtl -e -o ./jmeter_reports/PostgreSQL/TC#2/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -n -f -t ./jmeter_test_cases/PostgreSQL/TestCase#3_Postgre.jmx -l ./jmeter_test_result/PostgreSQL/TC#3/general_test_result.jtl -e -o ./jmeter_reports/PostgreSQL/TC#3/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -n -f -t ./jmeter_test_cases/PostgreSQL/TestCase#4_Postgre.jmx -l ./jmeter_test_result/PostgreSQL/TC#4/general_test_result.jtl -e -o ./jmeter_reports/PostgreSQL/TC#4/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -n -f -t ./jmeter_test_cases/PostgreSQL/TestCase#5_Postgre.jmx -l ./jmeter_test_result/PostgreSQL/TC#5/general_test_result.jtl -e -o ./jmeter_reports/PostgreSQL/TC#5/
+
+#java -jar ./jmeter/bin/ApacheJMeter.jar -n -f -t ./jmeter_test_cases/MS_Server/TestCase#1_SQLServer.jmx -l ./jmeter_test_result/MS_SQL/TC#1/general_test_result.jtl -e -o ./jmeter_reports/MS_SQL/TC#1/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -n -f -t ./jmeter_test_cases/MS_Server/TestCase#2_SQLServer.jmx -l ./jmeter_test_result/MS_SQL/TC#2/general_test_result.jtl -e -o ./jmeter_reports/MS_SQL/TC#2/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -n -f -t ./jmeter_test_cases/MS_Server/TestCase#3_SQLServer.jmx -l ./jmeter_test_result/MS_SQL/TC#3/general_test_result.jtl -e -o ./jmeter_reports/MS_SQL/TC#3/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -n -f -t ./jmeter_test_cases/MS_Server/TestCase#4_SQLServer.jmx -l ./jmeter_test_result/MS_SQL/TC#4/general_test_result.jtl -e -o ./jmeter_reports/MS_SQL/TC#4/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -f -t ./jmeter_test_cases/MS_Server/TestCase#5_SQLServer.jmx -l ./jmeter_test_result/MS_SQL/TC#5/general_test_result.jtl -e -o ./jmeter_reports/MS_SQL/TC#5/
+
+
+#java -jar ./jmeter/bin/ApacheJMeter.jar -f -t ./jmeter_test_cases/PostgreSQL/TestCase#4_Postgre.jmx -l ./jmeter_test_result/PostgreSQL/TC#1/general_test_result.jtl -e -o ./jmeter_reports/PostgreSQL/TC#1/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -f -t ./jmeter_test_cases/PostgreSQL/TestCase#2_Postgre.jmx -l ./jmeter_test_result/PostgreSQL/TC#2/general_test_result.jtl -e -o ./jmeter_reports/PostgreSQL/TC#2/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -f -t ./jmeter_test_cases/PostgreSQL/TestCase#3_Postgre.jmx -l ./jmeter_test_result/PostgreSQL/TC#3/general_test_result.jtl -e -o ./jmeter_reports/PostgreSQL/TC#3/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -f -t ./jmeter_test_cases/PostgreSQL/TestCase#4_Postgre.jmx -l ./jmeter_test_result/PostgreSQL/TC#4/general_test_result.jtl -e -o ./jmeter_reports/PostgreSQL/TC#4/
+java -jar ./jmeter/bin/ApacheJMeter.jar -f -t ./jmeter_test_cases/PostgreSQL/TestCase#5_Postgre.jmx -l ./jmeter_test_result/PostgreSQL/TC#5/general_test_result.jtl -e -o ./jmeter_reports/PostgreSQL/TC#5/
+
+#java -jar ./jmeter/bin/ApacheJMeter.jar -f -t ./jmeter_test_cases/MS_Server/TestCase#1_SQLServer.jmx -l ./jmeter_test_result/MS_SQL/TC#1/general_test_result.jtl -e -o ./jmeter_reports/MS_SQL/TC#1/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -f -t ./jmeter_test_cases/MS_Server/TestCase#2_SQLServer.jmx -l ./jmeter_test_result/MS_SQL/TC#2/general_test_result.jtl -e -o ./jmeter_reports/MS_SQL/TC#2/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -f -t ./jmeter_test_cases/MS_Server/TestCase#3_SQLServer.jmx -l ./jmeter_test_result/MS_SQL/TC#3/general_test_result.jtl -e -o ./jmeter_reports/MS_SQL/TC#3/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -f -t ./jmeter_test_cases/MS_Server/TestCase#4_SQLServer.jmx -l ./jmeter_test_result/MS_SQL/TC#4/general_test_result.jtl -e -o ./jmeter_reports/MS_SQL/TC#4/
+#java -jar ./jmeter/bin/ApacheJMeter.jar -f -t ./jmeter_test_cases/MS_Server/TestCase#5_SQLServer.jmx -l ./jmeter_test_result/MS_SQL/TC#5/general_test_result.jtl -e -o ./jmeter_reports/MS_SQL/TC#5/
