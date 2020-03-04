38e3ca79af606346ffcf6098bd397f45763f7ec8
ymaksymchuk <yehor.maksymchuk@avenga.com>
2020-03-05 00:54:41 +0200

    Finalize created tests

--- simulation_postgresql.sh
+++ simulation_postgresql.sh
@@ -2,6 +2,13 @@
 
 TEST_CASE_NUMBER=$1;
 USERS_COUNT=$2;
+RAMP_UP=$3;
+LOOPS=$4;
+USERS_COUNT_1=$5;
+USERS_COUNT_2=$6;
+USERS_COUNT_3=$7;
+USERS_COUNT_4=$8;
+
 TEST_CASE="";
 TEST_CASE_RESULT="";
 TEST_CASE_REPORT="";
@@ -32,10 +39,32 @@ case $TEST_CASE_NUMBER in
     TEST_CASE_REPORT=./jmeter_reports/PostgreSQL/TC#4/
     ;;
 
+  5)
+    TEST_CASE=./jmeter_test_cases/PostgreSQL/TestCase#5_Postgre.jmx
+    TEST_CASE_RESULT=./jmeter_test_result/PostgreSQL/TC#5/general_test_result.jtl
+    TEST_CASE_REPORT=./jmeter_reports/PostgreSQL/TC#5/
+    ;;
+
   *)
     echo "No test case was found!!"
     exit;
     ;;
 esac
-echo "java -jar ./jmeter/bin/ApacheJMeter.jar -Jusers_count=$USERS_COUNT -n -f -t $TEST_CASE -l $TEST_CASE_RESULT -e -o $TEST_CASE_REPORT"
-java -jar ./jmeter/bin/ApacheJMeter.jar -Jusers_count=$USERS_COUNT  -f -t $TEST_CASE -l $TEST_CASE_RESULT -e -o $TEST_CASE_REPORT
+echo "n\ ###########################################################################\n"
+echo "java -jar ./jmeter/bin/ApacheJMeter.jar \n
+-Jusers_count=$USERS_COUNT \n
+-Jramp_up=$RAMP_UP \n
+-Jloops=$LOOPS \n
+-Jusers_count_1=$USERS_COUNT_1 -Jusers_count_2=$USERS_COUNT_2 -Jusers_count_3=$USERS_COUNT_3 -Jusers_count_4=$USERS_COUNT_4 \n
+-n -f -t $TEST_CASE -l $TEST_CASE_RESULT -e -o $TEST_CASE_REPORT"
+echo "\n ########################################################################### \n"
+
+java -jar ./jmeter/bin/ApacheJMeter.jar \
+-Jusers_count=$USERS_COUNT \
+-Jramp_up=$RAMP_UP \
+-Jloops=$LOOPS \
+-Jusers_count_1=$USERS_COUNT_1 \
+-Jusers_count_2=$USERS_COUNT_2 \
+-Jusers_count_3=$USERS_COUNT_3 \
+-Jusers_count_4=$USERS_COUNT_4 \
+-n -f -t $TEST_CASE -l $TEST_CASE_RESULT -e -o $TEST_CASE_REPORT
