1752d334c5fdb973cd1f9bacc661f9aba79dd327
ymaksymchuk <yehor.maksymchuk@avenga.com>
2020-03-23 15:20:27 +0200

    Start finalizing

---
+++ run_sar.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#sudo apt-get install -y sysstat
+echo "Please check all logs file into the Simulation#$1 folder TC#$2"
+sar \-u 1 > ./Simulation\#$1/TC\#$2/cpu.txt &
+CPU=$!;
+        sar \-S 1 > ./Simulation\#$1/TC\#$2/swap.txt &
+SWAP=$!;
+        sar \-r 1 > ./Simulation\#$1/TC\#$2/ram.txt &
+RAM=$!;
+        sar \-d 1 > ./Simulation\#$1/TC\#$2/disk.txt &
+DISK=$!;
+        sar \-n IP 1 > ./Simulation\#$1/TC\#$2/net_ip.txt &
+IP=$!;
+        sar \-n EIP 1 > ./Simulation\#$1/TC\#$2/net_ip_error.txt &
+EIP=$!;
+        sar \-n TCP 1 > ./Simulation\#$1/TC\#$2/net_tcp.txt &
+TCP=$!
+wait $TCP $CPU $EIP $RAM $DISK $IP $EIP
