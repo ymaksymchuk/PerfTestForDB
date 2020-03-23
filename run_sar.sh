f705145d62922f2940f3b48008efe3adca08dc0f
ymaksymchuk <yehor.maksymchuk@avenga.com>
2020-03-23 18:03:38 +0200

    Finalise and add README.md for correct using

--- run_sar.sh
+++ run_sar.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #sudo apt-get install -y sysstat
-echo "Please check all logs file into the Simulation#$1 folder TC#$2"
+echo "Please check all txt file into the Simulation#$1 folder TC#$2"
 sar \-u 1 > ./Simulation\#$1/TC\#$2/cpu.txt &
 CPU=$!;
         sar \-S 1 > ./Simulation\#$1/TC\#$2/swap.txt &
--- simulation_postgresql_3.sh
+++ simulation_postgresql_3.sh
@@ -3,19 +3,18 @@
 TEST_CASE_NUMBER=${1};
 USERS_COUNT=${2};
 POOL_SIZE=${3};
-TIME=${4};
+CONNECTION_WAIT=${4};
+TIME=${5};
 
-USERS_COUNT_1=${5};
-USERS_COUNT_2=${6};
-USERS_COUNT_3=${7};
-USERS_COUNT_4=${8};
+USERS_COUNT_1=${6};
+USERS_COUNT_2=${7};
+USERS_COUNT_3=${8};
+USERS_COUNT_4=${9};
 
-POOL_SIZE_TC1=${9};
-POOL_SIZE_TC2=${10};
-POOL_SIZE_TC3=${11};
-POOL_SIZE_TC4=${12};
-
-CONNECTION_WAIT=${13};
+POOL_SIZE_TC1=${10};
+POOL_SIZE_TC2=${11};
+POOL_SIZE_TC3=${12};
+POOL_SIZE_TC4=${13};
 
 TEST_CASE="";
 TEST_CASE_RESULT="";
@@ -72,6 +71,7 @@ esac
 
 echo "-Jusers_count=$USERS_COUNT \n
       -Jpool_size=$POOL_SIZE \n
+      -Jconnection_wait=$CONNECTION_WAIT \n
       -Time=$TIME \n
       -Test_Case=$TEST_CASE \n
       -Test Report=$TEST_CASE_REPORT \n
@@ -83,13 +83,13 @@ echo "-Jusers_count=$USERS_COUNT \n
       -Jpool_size_tc1=$POOL_SIZE_TC1 \n
       -Jpool_size_tc2=$POOL_SIZE_TC2 \n
       -Jpool_size_tc3=$POOL_SIZE_TC3 \n
-      -Jpool_size_tc4=$POOL_SIZE_TC4 \n\
-      -Jconnection_wait=$CONNECTION_WAIT \n"
+      -Jpool_size_tc4=$POOL_SIZE_TC4 \n\"
 
 java -jar ./jmeter/bin/ApacheJMeter.jar \
 -Jusers_count=$USERS_COUNT \
 -Jresult_folder=$RESULT_FOLDER \
 -Jpool_size=$POOL_SIZE \
+-Jconnection_wait=$CONNECTION_WAIT \
 -Jtime=$TIME \
 -Jusers_count_tc1=$USERS_COUNT_1 \
 -Jusers_count_tc2=$USERS_COUNT_2 \
@@ -99,6 +99,5 @@ java -jar ./jmeter/bin/ApacheJMeter.jar \
 -Jpool_size_tc2=$POOL_SIZE_TC2 \
 -Jpool_size_tc3=$POOL_SIZE_TC3 \
 -Jpool_size_tc4=$POOL_SIZE_TC4 \
--Jconnection_wait=$CONNECTION_WAIT \
  -n -f -t $TEST_CASE -l $TEST_CASE_RESULT -e -o $TEST_CASE_REPORT
 
